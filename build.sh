bcb8a0f484d7fa8ce1fa7a2d12e7a073f9ceda95
ThiagoAlmeidaSiqueira <thiagoalmeidasiqueira@gmail.com>
2025-09-08 17:37:44 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 docker build --no-cache -t n8n-nodes-docling-serve .
+docker create --name temp-container n8n-nodes-docling-serve
+docker cp temp-container:/home/node/.n8n/custom/DoclingServe.node.js ./
+docker rm temp-container
